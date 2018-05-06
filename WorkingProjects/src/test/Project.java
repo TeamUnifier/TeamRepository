cc368c480b0564de52f7bdb0e9531e089a23383e
JitenderPal <palljiten@gmail.com>
2018-05-07 01:22:20 +0530

    commit 1

---
+++ WorkingProjects/src/test/Project.java
@@ -0,0 +1,22 @@
+package test;
+
+public class Project 
+{
+  int x;
+  int y;
+  public int getX() {
+	return x;
+}
+public void setX(int x) {
+	this.x = x;
+}
+public int getY() {
+	return y;
+}
+public void setY(int y) {
+	this.y = y;
+}
+
+  
+  
+}
